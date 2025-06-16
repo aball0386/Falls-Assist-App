79f5778a91006cf2b449e7f7f912aea8540068ed
aball0386 <aaronball8145@gmail.com>
2025-06-16 14:10:44 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,33 +4,23 @@ const urlsToCache = [
   './index.html',
   './style.css',
   './app.js',
+  './manifest.json',
   './nhs.png',
   './secamb.png',
-  './manifest.json'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install event - cache files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event - clean old caches if any
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-// Fetch event - respond with cached resources or fetch network
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
